ff7220fe083e80ed5feff1110cadfbdfc9cbd5cd
yuslandik <61959296+yuslandik@users.noreply.github.com>
2020-03-29 18:52:44 -0700

    Add files via upload

---
+++ tugas_alpro_2_6.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main (){
+	char text1[]="katak";
+	char text2[]="malam";
+	char text3[]="ada";
+	
+	strrev(text1);
+	cout<<text1<<endl;
+	
+	strrev(text2);
+	cout<<text2<<endl;
+	
+	strrev(text3);
+	cout<<text3<<endl;
+}
